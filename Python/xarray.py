60b212e53d1f27fbdc519b0057a8c7c44f8f4344
Matthias Mohr <m.mohr@uni-muenster.de>
2021-12-14 16:13:26 +0100

    SOLUTION!!!

--- Python/xarray.py
+++ Python/xarray.py
@@ -2,10 +2,12 @@ import time
 import xarray as xr
 import numpy as np
 import rpy2.robjects as robjects
+from array import array
 
 # Create example numpy and xarray data
 npData = np.random.rand(5,4,3)
 xrData = xr.DataArray(npData, dims = ['x', 'y', 'b'], coords = {'x': [1,2,3,4,5], 'y': [6,7,8,9], 'b': ['b1', 'b2', 'b3']})
+xrDataset = xrData.to_dataset(dim = 'b')
 
 # Read R Code
 file = open('./R/vectors.R', mode = 'r')
@@ -14,14 +16,16 @@ file.close()
 
 # "Compile" R code once
 rEnv = robjects.r(rCode)
-reducerRFun = robjects.globalenv['reducer']
-reducer = lambda data, axis: data.tolist()
+reducer = robjects.globalenv['reducer']
+def ufunc(data):
+	vector = reducer(array('f', data), context)
+	return vector[0]
+
 context = -1
 
 # Run R code
 t1 = time.time() # Start benchmark
-# groups = xrData.groupby(dim = 'b');
-result = xrData.reduce(reducer, dim = 'b')
+result = xr.apply_ufunc(ufunc, xrData, vectorize = True, input_core_dims = [['b']])
 t2 = time.time() # End benchmark
 
 # Print result and benchmark
--- R/vectors.R
+++ R/vectors.R
@@ -1,4 +1,3 @@
 reducer <- function(data, context = NULL) {
-  print(data)
   mean(data) * context
 }
