cd8732103697c022ee0e11caf58bfd3d20c518b8
Matthias Mohr <m.mohr@uni-muenster.de>
2022-02-15 16:49:15 +0100

    Add comment

--- udfs/reduce.R
+++ udfs/reduce.R
@@ -1,6 +1,7 @@
 # This is the function that is passed to st_apply for reduction of a dimension
 
 # vectorized
+# document that data is a list and do.call might be useful
 udf = function(data, context = NULL) {
   do.call(pmax, data) * context
 }
