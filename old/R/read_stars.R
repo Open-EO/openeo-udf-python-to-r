0822e7085407642b3ca49a4e5b852ce600c86cae
Matthias Mohr <m.mohr@uni-muenster.de>
2022-02-16 13:27:02 +0100

    clean-up

--- old/R/read_stars.R
+++
@@ -1,3 +0,0 @@
-library("stars")
-tif = system.file("tif/L7_ETMs.tif", package = "stars")
-x = read_stars(tif)
--- old/R/vectors.R
+++
@@ -1,4 +0,0 @@
-# This is the function that is passed to st_apply for reduction of a dimension
-reducer = function(data, context = NULL) {
-  mean(data) * context
-}
