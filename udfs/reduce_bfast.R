06214098f333484b022e4d7525e317072f6b981a
Matthias Mohr <m.mohr@uni-muenster.de>
2022-02-17 17:11:04 +0100

    Separate bfast test

--- udfs/reduce_bfast.R
+++ udfs/reduce_bfast.R
@@ -1,9 +1,18 @@
 udf = function(data, context = NULL) {
-  pixels = unlist(data)
+  pixels = unlist(data) # this are 150000 pixels right now. why? Should be 500
   dates = names(data)
-  start_monitor = if(is.null(context) || is.null(context$start_monitor)) 2018 else context$start_monitor
+
+  start_monitor = if(is.null(context) || is.null(context$start_monitor)) 2021 else context$start_monitor
   val = if(is.null(context) || is.null(context$val)) "breakpoint" else context$val
   level = if(is.null(context) || is.null(context$level) || length(context$val) == 0) c(0.05, 0.05) else context$level
+
+  # error handling
+  stopifnot(length(pixels) == length(dates)) 
+  stopifnot(val %in% c("breakpoint", "magnitude"))
+  # make sure there are enough observations
+  if (sum(!is.na(lsts)) < 100){
+    return(NA)
+  }
 
   # create ts object for bfast
   lsts = bfast::bfastts(pixels, dates, type = c("irregular"))
@@ -25,10 +34,3 @@ udf = function(data, context = NULL) {
 
 # unnecessary stuff removed from fun
   #error handling
-  #stopifnot(length(pixels) == length(dates)) 
-  #stopifnot(val %in% c("breakpoint", "magnitude"))
-  
-  # make sure there are enough observations
-  #if (sum(!is.na(lsts)) < 100){
-  #  return(NA)
-  #}
