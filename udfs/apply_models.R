874c8279528133066d5dbab09ddba3de4f00871f
Mattia Rossi <mattia.rossi@eurac.edu>
2022-06-23 11:36:06 +0000

    Apply Model script

---
+++ udfs/apply_models.R
@@ -0,0 +1,51 @@
+udf=function(data,context){
+  
+  #url<-'https://github.com/Open-EO/openeo-udf-python-to-r/blob/main/models/TestModel1.rds'
+  #model<-RCurl::getURL(url)
+  return(data)
+  #return(data)
+  
+}
+
+
+
+# udf_chunked = function(data, context) {
+#   pixels = unlist(data) # this are 150000 pixels right now. why? Should be 500
+#   dates = names(data)
+# 
+#   val = if(is.null(context) || is.null(context$val)) "breakpoint" else context$val
+#   level = if(is.null(context) || is.null(context$level) || length(context$val) == 0) c(0.05, 0.05) else context$level
+# 
+#   # error handling
+#   stopifnot(length(pixels) == length(dates)) 
+#   stopifnot(val %in% c("breakpoint", "magnitude"))
+# 
+#   # create ts object for bfast
+#   lsts = bfast::bfastts(pixels, dates, type = c("irregular"))
+# 
+#   # make sure there are enough observations
+#   if (sum(!is.na(lsts)) < 100){
+#     return(NA)
+#   }
+#  
+#   # run bfast
+#   res = bfast::bfastmonitor(lsts, 
+#                      context$start_monitor, 
+#                      formula = response~harmon, 
+#                      order = 1, 
+#                      history = "all", 
+#                      level = level,
+#                      verbose = F)[[val]]
+#   if(is.na(res)){
+#     return(0)
+#   }
+#   return(res) 
+# }
+# 
+# udf_setup = function(context) {
+#   print("setup");
+# }
+# 
+# udf_teardown = function(context) {
+#   print("teardown");
+# }
