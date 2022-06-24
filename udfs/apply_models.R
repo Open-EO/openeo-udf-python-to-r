6d1d611f1457b1f1f00059e6dfbdac13e0b771c1
Mattia Rossi <mattia.rossi@eurac.edu>
2022-06-24 08:37:10 +0000

    Updated ML udf

--- udfs/apply_models.R
+++ udfs/apply_models.R
@@ -1,51 +1,30 @@
-udf=function(data,context){
+udf = function(data,context){
+
+  # read a machine learning model done with caret
+  url1      <- 'https://github.com/Open-EO/openeo-udf-python-to-r/blob/UC1_ML/models/TestModel1.rds?raw=True'
+  getModel  <- readRDS(gzcon(url(url1)))
   
-  #url<-'https://github.com/Open-EO/openeo-udf-python-to-r/blob/main/models/TestModel1.rds'
-  #model<-RCurl::getURL(url)
-  return(data)
-  #return(data)
+  print(paste(getModel$modelInfo$label,"- Model Loaded successfully"))
   
+  # saveRDS(data,"models/testdata.rds")
+
+  # convert the data xarray to raster
+  data<-readRDS("models/testdata.rds")
+  if(class(data)=="stars") print("Data converted to Stars object") else stop()
+
+  # STARS Package prediction
+  data.split<-split(data,"var")
+  names(data.split)<-caret::predictors(getModel)
+  prediction.stars <-predict(data.split,getModel)
+  
+  # RASTER package prediction
+  #s<-data[[1]]
+  #st<-raster::brick(s)
+  #names(st)=caret::predictors(getModel)
+  #prediction.raster<-predict(st,getModel)
+  print("Prediction Done")
+  write_stars(prediction.stars,"models/starsprediction2.nc")
+
+  # Return the raster
+  return(prediction.stars)
 }
-
-
-
-# udf_chunked = function(data, context) {
-#   pixels = unlist(data) # this are 150000 pixels right now. why? Should be 500
-#   dates = names(data)
-# 
-#   val = if(is.null(context) || is.null(context$val)) "breakpoint" else context$val
-#   level = if(is.null(context) || is.null(context$level) || length(context$val) == 0) c(0.05, 0.05) else context$level
-# 
-#   # error handling
-#   stopifnot(length(pixels) == length(dates)) 
-#   stopifnot(val %in% c("breakpoint", "magnitude"))
-# 
-#   # create ts object for bfast
-#   lsts = bfast::bfastts(pixels, dates, type = c("irregular"))
-# 
-#   # make sure there are enough observations
-#   if (sum(!is.na(lsts)) < 100){
-#     return(NA)
-#   }
-#  
-#   # run bfast
-#   res = bfast::bfastmonitor(lsts, 
-#                      context$start_monitor, 
-#                      formula = response~harmon, 
-#                      order = 1, 
-#                      history = "all", 
-#                      level = level,
-#                      verbose = F)[[val]]
-#   if(is.na(res)){
-#     return(0)
-#   }
-#   return(res) 
-# }
-# 
-# udf_setup = function(context) {
-#   print("setup");
-# }
-# 
-# udf_teardown = function(context) {
-#   print("teardown");
-# }
