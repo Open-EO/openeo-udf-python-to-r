f903a2ba3dbf269cfc374e1ba0965549bb20692c
Matthias Mohr <m.mohr@uni-muenster.de>
2022-10-26 18:17:49 +0200

    Fix typo

--- tests/udfs/apply_dimension.R
+++ tests/udfs/apply_dimension.R
@@ -3,7 +3,7 @@
 # udf_chunked = function(data, context) {
 #   avg_data = c()
 #   for (i in 1:length(data)) {
-#     start = max(1, i-context)?
+#     start = max(1, i-context)
 #     end = min(length(data), i+context)
 #     avg_data[i] = mean(data[start:end])
 #   }
